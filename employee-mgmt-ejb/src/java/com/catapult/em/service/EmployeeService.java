2927a84ed7ae61dc760c74b91b9a6056c874b7dd
Jayrome Vergara <jaycverg@gmail.com>
2012-11-19 20:00:40 +0800

    simplified the project for demo

--- employee-mgmt-ejb/src/java/com/catapult/em/service/EmployeeService.java
+++ employee-mgmt-ejb/src/java/com/catapult/em/service/EmployeeService.java
@@ -1,17 +1,11 @@
 package com.catapult.em.service;
 
 import com.catapult.em.model.Employee;
-import com.catapult.em.util.CollectionUtil;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.ejb.Local;
-import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 /**
  *
@@ -20,8 +14,7 @@ import javax.persistence.Query;
 
 @Stateless
 @Local(EmployeeServiceLocal.class)
-@Remote(EmployeeServiceRemote.class)
-public class EmployeeService implements EmployeeServiceLocal, EmployeeServiceRemote
+public class EmployeeService implements EmployeeServiceLocal
 {
     @PersistenceContext
     private EntityManager em;
@@ -50,93 +43,16 @@ public class EmployeeService implements EmployeeServiceLocal, EmployeeServiceRem
     @Override
     public void remove(long id)
     {
-        //em.removeAll(emp); -- object is detached
         em.createQuery("delete from Employee where id=:id")
           .setParameter("id", id)
           .executeUpdate();
     }
     
     @Override
-    public void removeAll(List<Long> ids)
-    {
-        em.createQuery("delete from Employee where id in :idList")
-          .setParameter("idList", ids)
-          .executeUpdate();
-    }
-    
-    @Override
     public List<Employee> getList()
     {
-        return getList(null);
+        return em.createQuery("select o from Employee o")
+                 .getResultList();
     }
 
-    @Override
-    public List<Employee> getList(Map param)
-    {
-        if( param == null ) param = new HashMap();
-        
-        StringBuilder buff = new StringBuilder();
-        buff.append("select o from Employee o");
-        if( param.get("sortedBy") != null ) {
-            buff.append(" order by " + param.get("sortedBy"));
-        }
-        else {
-            buff.append(" order by id");
-        }
-        
-        if( "false".equals(param.get("ascending") + "")) {
-            buff.append(" desc ");
-        }
-        
-        Query qry = em.createQuery(buff.toString());
-        
-        int start = 0;
-        int limit = -1;        
-        if( param.get("start") != null ) 
-            start = (Integer) param.get("start");
-        if( param.get("limit") != null)
-            limit = (Integer) param.get("limit");
-        
-        if( start >= 0 ) qry.setFirstResult(start);
-        if( limit > -1 ) qry.setMaxResults(limit);
-        
-        return qry.getResultList();
-    }
-
-    @Override
-    public List<Employee> find(String lastName, String firstName)
-    {
-        return find(lastName, firstName, 50);
-    }
-
-    @Override
-    public List<Employee> find(String lastName, String firstName, int maxResults)
-    {
-        StringBuilder buff = new StringBuilder();
-        buff.append("select o from Employee o");
-        
-        List cond = new ArrayList();
-        if( lastName != null )  cond.add("lastName like :lastName");
-        if( firstName != null ) cond.add("firstName like :firstName");
-        
-        if( cond.size() > 0 ){
-            buff.append(" where ");
-            buff.append( CollectionUtil.join(cond, " and "));
-        }
-        
-        Query qry = em.createQuery(buff.toString());
-        if( lastName != null )  qry.setParameter("lastName", lastName+"%");
-        if( firstName != null ) qry.setParameter("firstName", firstName+"%");
-        
-        if( maxResults > 0 ) qry.setMaxResults(maxResults);
-        
-        return qry.getResultList();
-    }
-
-    @Override
-    public long getRowCount()
-    {
-        return (Long) em.createQuery("select count(o) from Employee o").getSingleResult();
-    }
-    
 }
--- employee-mgmt-ejb/src/java/com/catapult/em/service/EmployeeServiceLocal.java
+++ employee-mgmt-ejb/src/java/com/catapult/em/service/EmployeeServiceLocal.java
@@ -20,15 +20,5 @@ public interface EmployeeServiceLocal
 
     void remove(long id);
 
-    void removeAll(List<Long> ids);
-
     List<Employee> getList();
-
-    List<Employee> getList(Map param);
-
-    List<Employee> find(String lastname, String firstname);
-
-    List<Employee> find(String lastname, String firstname, int maxResults);
-
-    long getRowCount();
 }
